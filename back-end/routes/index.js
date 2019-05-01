70836eb695036c94fade9550801cf511d69a3090
flaviomau <flaviomau@yahoo.com>
2019-04-30 22:21:45 -0300

    Movie routes created

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -1,8 +1,17 @@
-const express     = require('express'),
-      router      = express.Router()
+const express = require('express'),
+      router  = express.Router(),
+      mongodb = require('../db/mongodb/mongoDbStrategy'),
+      context = require('../db/base/contextStrategy')
+
+const contextDatabase = new context(new mongodb())
+contextDatabase.connect()
+const MovieModel = require('../models/MovieModel')
+const MovieRoutes = require('./movies')
 
 router.get('/', function (request, response) {
   response.send('Upcoming Movies API')
 })
+
+router.use('/movies', MovieRoutes(new MovieModel(contextDatabase)))
 
 module.exports = router
