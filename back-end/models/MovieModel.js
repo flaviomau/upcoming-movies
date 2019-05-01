72d3b49e57c8a84a9a26a1de9916022e566af2b6
flaviomau <flaviomau@yahoo.com>
2019-04-30 22:04:46 -0300

    Movie model created

---
+++ back-end/models/MovieModel.js
@@ -0,0 +1,35 @@
+'use strict'
+
+class MovieDao {
+
+  constructor(db) {
+    this._db = db
+  }
+
+  async create(item){
+    return this._db.create(item)
+  }
+  
+  async find(query){
+    return this._db.read(query)
+  }
+  
+  async findOne(item){
+    const movie = await this._db.read(item)
+    if(movie.length == 0)
+      return null
+    else{
+      return movie[0]
+    }      
+  }
+  
+  async update(id_movie, item){
+    return this._db.update(id_movie, item)
+  }
+  
+  async remove(id_movie){
+    return this._db.delete(id_movie)
+  }
+}
+
+module.exports = MovieDao
